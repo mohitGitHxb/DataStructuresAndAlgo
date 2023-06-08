2bf956041c5b498a6160e93d420e5b720dd60217
mohitGitHxb <mohit2003second@gmail.com>
2023-06-08 18:02:10 +0530

    Backtracking and Recursion COMPLETED

--- Recursions/RecursionTheory.cpp
+++ Recursions/RecursionTheory.cpp
@@ -1213,13 +1213,39 @@ public:
                 }
             }
         }
-            return true;
+        return true;
     }
     void solveSudoku(vector<vector<char>> &board)
     {
         solve(board);
     }
 };
+
+//^ Kth permutation of a number 1 -> N
+string getPermutation(int n, int k)
+{
+    int fact = 1;
+    vector<int> numbers;
+    string ans = "";
+    for (int i = 1; i < n; i++)
+    {
+        fact *= i;
+        numbers.emplace_back(i);
+    }
+    numbers.push_back(n);
+    k--;
+    while (true)
+    {
+        ans += to_string(numbers[k / fact]);
+        numbers.erase(numbers.begin() + k / fact);
+        if (numbers.size() == 0)
+            break;
+
+        k = k % fact;
+        fact /= numbers.size();
+    }
+    return ans;
+}
 int main()
 {
 

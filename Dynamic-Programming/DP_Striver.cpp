beada27a52a7f2342a0d8875e137cf99b7b785f0
mohitGitHxb <mohit2003second@gmail.com>
2023-06-11 08:45:14 +0530

    1-D Dp problem added

--- Dynamic-Programming/DP_Striver.cpp
+++ Dynamic-Programming/DP_Striver.cpp
@@ -245,7 +245,7 @@ public:
 
     //^ Follow up Question : allowed K jumps
 
-        int frogJump(int n, vector<int> &heights, int k)
+    int frogJump(int n, vector<int> &heights, int k)
     {
 
         vector<int> dp(n, INT_MAX);
@@ -267,6 +267,259 @@ public:
         }
 
         return dp[n - 1];
+    }
+};
+
+//^ Maximum sum of array such that no two elements are adjacent to each other
+class HouseRobber
+{
+private:
+public:
+    int n;
+    vector<int> dp;
+    HouseRobber(int n)
+    {
+        this->n = n;
+        this->dp.resize(n, -1);
+    }
+    int solve(vector<int> &arr, int idx)
+    {
+        if (idx == 0)
+            return arr[idx];
+        if (idx < 0)
+            return 0;
+        int pick = arr[idx] + solve(arr, idx - 2);
+        int notpick = 0 + solve(arr, idx - 1);
+        return max(pick, notpick);
+    }
+
+    int solve_memo(vector<int> &arr, int idx)
+    {
+        if (idx == 0)
+            return arr[idx];
+        if (idx < 0)
+            return 0;
+        if (dp[idx] != -1)
+        {
+            return dp[idx];
+        }
+        int pick = arr[idx] + solve_memo(arr, idx - 2);
+        int notpick = 0 + solve_memo(arr, idx - 1);
+        return dp[idx] = max(pick, notpick);
+    }
+    int solve_tabular(vector<int> &arr, int idx)
+    {
+        dp.at(0) = arr.at(0);
+        for (int i = 1; i < arr.size(); i++)
+        {
+            int pick = (i > 1) ? arr.at(i) + dp[i - 2] : arr.at(i);
+            int notPick = 0 + arr.at(i - 1);
+
+            dp.at(i) = max(pick, notPick);
+        }
+        return dp.back();
+    }
+    int solve_optimized(vector<int> &arr, int idx)
+    {
+        int prev1 = arr.front();
+        int prev2 = 0;
+        for (int i = 1; i < arr.size(); i++)
+        {
+            int pick = (i > 1) ? arr.at(i) + prev2 : arr.at(i);
+            int notPick = 0 + prev1;
+            int curr = max(pick, notPick);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+    //^ House Robber 2 [Follow up] what if the last house is adjacent to the first house
+    int maximumSumWithoutRotation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int take = nums[i];
+            if (i > 1)
+                take += prev2;
+
+            int notPick = 0 + prev;
+
+            int curr = max(take, notPick);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+    int rob(vector<int> &nums)
+    {
+        vector<int> temp1, temp2;
+        if (nums.size() == 1)
+            return nums.front();
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i != 0)
+                temp1.push_back(nums.at(i));
+            if (i != nums.size() - 1)
+                temp2.push_back(nums.at(i));
+        }
+
+        return max(maximumSumWithoutRotation(temp1), maximumSumWithoutRotation(temp2));
+    }
+};
+
+//^ Ninja's training ( Similar to House Robber but in 2D)
+class Training
+{
+private:
+    /*
+&    Step 1: Express the problem in terms of indexes.
+
+&Now given an example, what is the one clear parameter which breaks the problem in different steps?
+It is the number of days. Clearly, we have n days (from 0 to n-1), so one changing parameter which can be expressed in terms of indexes is ‘day’.
+
+&Every day we have the option of three activities(say 0,1,2) to be performed. Suppose that we are at a day_i ( shown in the fig) in the example given below. What is one more parameter along with the day that we must know to try out the correct choices at day_i?
+
+&It is the ‘last’ choice that we tried out on day_i+1 ( i+1 in case of top-down recursion). Unless we know the last choice we made, how can we decide whether a choice we are making is correct or not?
+&Now there are three options each day(say 0,1,2) which becomes the ‘last’ of the next day. If we are just starting from the day_n-1, then for the day_n-1 we can try all three options. We can say ‘last’ for day_n-1 is 3.
+
+&Therefore our function will take two parameters – day and last.
+
+&   Step 2: Try out all possible choices at a given index.
+
+&   We are writing a top-down recursive function. We start from day_n-1 to day_0. Therefore whenever we call the recursive function for the next day we call it for f(day-1, //second parameter).
+&   Now let’s discuss the second parameter. The choices we have for a current day depend on the ‘last’ variable. If we are at our base case (day=0), we will have the following choices.
+
+&   Other than the base case, whenever we perform an activity ‘i’ its merit points will be given by points[day][i] and to get merit points of the remaining days we will let recursion do its job by passing f(d-1, i). We are passing the second parameter as i because the current day’s activity will become the next day’s last.
+
+Step 3:  Take the maximum of all choices
+
+& As the problem statement wants us to find the maximum merit points, we will take the maximum of all choices.
+
+
+Steps to memoize a recursive solution:
+
+& If we draw the recursion tree, we will see that there are overlapping subproblems. In order to convert a recursive solution to the following steps will be taken:
+
+&     Create a dp array of size [n][4]. There are total ‘n’ days and for every day, there can be 4 choices (0,1,2 and 3). Therefore we take the dp array as dp[n][4].
+&    Whenever we want to find the answer of particular parameters (say f(day,last)), we first check whether the answer is already calculated using the dp array(i.e dp[day][last]!= -1 ). If yes, simply return the value from the dp array.
+&    If not, then we are finding the answer for the given value for the first time, we will use the recursive relation as usual but before returning from the function, we will set dp[day][last] to the solution we get.
+s
+     */
+public:
+    int f(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp)
+    {
+
+        if (dp[day][last] != -1)
+            return dp[day][last];
+
+        if (day == 0)
+        {
+            int maxi = 0;
+            for (int i = 0; i <= 2; i++)
+            {
+                if (i != last)
+                    maxi = max(maxi, points[0][i]);
+            }
+            return dp[day][last] = maxi;
+        }
+
+        int maxi = 0;
+        for (int i = 0; i <= 2; i++)
+        {
+            if (i != last)
+            {
+                int activity = points[day][i] + f(day - 1, i, points, dp);
+                maxi = max(maxi, activity);
+            }
+        }
+
+        return dp[day][last] = maxi;
+    }
+
+    /*
+    @ Tabulation
+    &    Declare a dp[] array of size [n][4]
+&    First initialize the base condition values. We know that base condition arises when day = 0. Therefore, we can say that the following will be the base conditions
+&        dp[0][0] = max(points[0][1], points[0][2])
+&        dp[0][1] = max(points[0][0], points[0][2])
+&        dp[0][2] = max(points[0][0], points[0][1])
+&        dp[0][3] = max(points[0][0], points[0][1] and points[0][2])
+&    Set an iterative loop which traverses dp array (from index 1 to n) and for every index set its value according to the recursive logic
+
+* O(12N) T.C | O(4N) S.C
+     */
+
+    int ninjaTraining(int n, vector<vector<int>> &points)
+    {
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] = max(points[0][1], points[0][2]);
+        dp[0][1] = max(points[0][0], points[0][2]);
+        dp[0][2] = max(points[0][0], points[0][1]);
+        dp[0][3] = max(points[0][0], max(points[0][2], points[0][1]));
+        for (int day = 1; day < n; day++)
+        {
+            for (int last = 0; last < 4; last++)
+            {
+                dp[day][last] = 0;
+
+                for (int task = 0; task < 3; task++)
+                {
+                    int point = points[day][task] + dp[day - 1][task];
+                    dp[day][last] = max(dp[day][task], point);
+                }
+            }
+        }
+        return dp[n - 1][3];
+    }
+    /*
+    @ Space Optimization
+~    If we closely look the relation,
+
+&dp[day][last] =  max(dp[day][last],points[day][task] + dp[day-1][task])
+
+&Here the task can be anything from 0 to 3 and day-1 is the previous stage of recursion. So in order to compute any dp array value, we only require the last row to calculate it.
+
+&    So rather than storing the entire 2D Array of size N*4, we can just store values of size 4(say prev).
+&    We can then take a dummy array, again of size 4 (say temp) and calculate the next row’s value using the array we stored in step 1.
+&    After that whenever we move to the next day, the temp array becomes our prev for the next step.
+&    At last prev[3] will give us the answer.
+* O(12N) T.C | O(4) S.C
+     */
+
+    int ninjaTraining_optimized(int n, vector<vector<int>> &points)
+    {
+
+        vector<int> prev(4, 0);
+
+        prev[0] = max(points[0][1], points[0][2]);
+        prev[1] = max(points[0][0], points[0][2]);
+        prev[2] = max(points[0][0], points[0][1]);
+        prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+        for (int day = 1; day < n; day++)
+        {
+
+            vector<int> temp(4, 0);
+            for (int last = 0; last < 4; last++)
+            {
+                temp[last] = 0;
+                for (int task = 0; task <= 2; task++)
+                {
+                    if (task != last)
+                    {
+                        temp[last] = max(temp[last], points[day][task] + prev[task]);
+                    }
+                }
+            }
+
+            prev = temp;
+        }
+
+        return prev[3];
     }
 };
 int main(int argc, char const *argv[])

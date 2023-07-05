065c713131034d8bb47f4c174e5af626aed1b9df
mohitGitHxb <mohit2003second@gmail.com>
2023-07-05 08:11:26 +0530

    Added more trees problems to

--- SHEETs/BinaryTrees_450.cpp
+++ SHEETs/BinaryTrees_450.cpp
@@ -339,6 +339,67 @@ public:
         return dia;
     }
 };
+
+//^ 8 Mirror of a tree
+/*
+& swap left and right pointers
+* O(N) T.C | O(H) S.C
+ */
+Node *Mirror(Node *root)
+{
+    if (!root)
+        return root;
+    swap(root->left, root->right);
+    if (root->left)
+        Mirror(root->left);
+    if (root->right)
+        Mirror(root->right);
+    return root;
+}
+
+//^ 9 Minimum path sum in a binary tree
+class MinPathSum
+{
+private:
+    /*
+        The solve function takes a TreeNode pointer root and a reference to an integer maxi (initialized to a very small value) as parameters.
+
+    If root is nullptr, indicating an empty node, the function returns 0.
+
+    The function recursively calls solve on the left and right subtrees of root, storing the returned values in the variables left and right, respectively. The values are calculated by taking the maximum between 0 and the result of the recursive calls. This ensures that negative values are not considered in the path sum.
+
+    The maximum path sum that includes the current root node is calculated as left + right + root->val. This is compared with the current maximum sum maxi, and the greater value is stored in maxi. This step finds the maximum sum path that includes the current node.
+
+    The function returns the maximum path sum starting from the current root node, which is calculated as root->val + max(right, left). This step considers whether to include the current node in the path sum or not, by choosing the maximum between the sums of the left and right subtrees.
+
+    The maxPathSum function initializes the maxi variable to a very small value.
+
+    It calls the solve function with the root node and the maxi variable as parameters. This updates the maxi variable with the maximum path sum in the tree.
+
+    Finally, the maxi variable, which now holds the maximum path sum, is returned as the result of the function.
+
+The time complexity of this code is O(N), where N is the number of nodes in the binary tree. This is because we perform a DFS traversal of the tree, visiting each node once.
+
+The space complexity is O(H), where H is the height of the binary tree. This is because the recursion stack will have at most H frames at any given time, corresponding to the maximum depth of the tree. In the worst case, when the tree is skewed, the height can be equal to N, resulting in a space complexity of O(N). However, in a balanced tree, the height is typically logarithmic in the number of nodes, resulting in a space complexity of O(log N).
+    * O(N) T.C | O(N) S.C
+     */
+public:
+    int solve(TreeNode *root, int &maxi)
+    {
+        if (!root)
+            return 0;
+        int left = max(0, solve(root->left, maxi));
+        int right = max(0, solve(root->right, maxi));
+        maxi = max(maxi, left + right + root->val);
+        return root->val + max(right, left);
+    }
+    int maxPathSum(TreeNode *root)
+    {
+        int maxi = -1e8;
+        solve(root, maxi);
+        return maxi;
+    }
+};
 int main()
 {
     return 0;

1d2f7e2a00cff1197a0b0c26a6c800e3b9104845
mohitGitHxb <mohit2003second@gmail.com>
2023-10-17 09:19:07 +0530

    Added questions

--- SHEETs/StacksAndQueues_450.cpp
+++ SHEETs/StacksAndQueues_450.cpp
@@ -1,7 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//^ 1 Implement a stack using array
+//^ 1 Valid parenthesis
+
+class ValidParenthesis
+{
+    public:
+    //Function to check if brackets are balanced or not.
+     bool isMatching(char a,char b){
+    return ((a=='('&&b==')'))||((a=='{'&&b=='}'))||((a=='['&&b==']'));
+    }
+    bool ispar(const string &str)
+    {
+        if(str.empty())return true;
+        // Your code here
+        stack<char> s;
+        // s.push(x[0]);
+ for (int i = 0; i < str.length(); i++)
+    {
+        if(str.at(i)=='('||str.at(i)=='{'||str.at(i)=='['){
+            s.push(str[i]);
+        }
+        else{
+            if(s.empty())
+                return false;
+            else if(isMatching(s.top(),str[i])==false)
+                return false;
+            else 
+                s.pop();
+        }
+    }
+    return s.empty();
+        
+    }
+
+};
+
+//^ 2 Implementation of min stack
+class MinStack{
+    int minEle;
+    stack<pair<int,int>> s;
+    public:
+    
+       /*returns min element from stack*/
+       int getMin(){
+           
+           //Write your code here
+           if(s.empty()) return -1;
+           minEle = s.top().second;
+           return minEle;
+       }
+       
+       /*returns poped element from stack*/
+       int pop(){
+           if(s.empty()) return -1;
+           int element = s.top().first;
+           s.pop();
+           return element;
+       }
+       
+       /*push element x into the stack*/
+       void push(int x){
+           
+           if(s.empty()){
+               s.push({x,x});
+           }else{
+               s.push({x,min(s.top().second,x)});
+           }
+       }
+};
 
 int main()
 {

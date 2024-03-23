1906b8029d760376815ba7f6c5d8271f9a9a19f4
mohitGitHxb <mohit2003second@gmail.com>
2024-03-23 09:46:26 +0530

    Added questions.

---
+++ Competitive Programming/Greedy/1946B Maximum sum.cpp
@@ -0,0 +1,307 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cerr << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int> hmpi;
+typedef unordered_map<ll, ll> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int> hsi;
+typedef unordered_set<ll> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+#define sz(x) ((ll)(x).size())
+
+vpii dirs4 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+vpii dirs8 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cerr << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cerr << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cerr << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cerr << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+/*
+    * Greedy and mathematical approach to solve this problem.
+    & find the maximum subarray sum using KADANE's algorithm
+    & The most optimal way to increase the overall sum of the array by insert this maxSubarraySum right next to the subarray which gave us this sum
+    & [-1 2 3 -4 -10] here maxSubarraySum = 5 and subarray = [2 3] so we insert 5 right next to this subarray
+    & [-1 2 3 5 -4 -10] now we again get the maxSubarraySum = 10 which can be broken down as : [5 + 5]
+    % Simply replicate this and we obtain a geometric progression type series.
+ */
+
+void solve()
+{
+    int mod = 1e9 + 7;
+    ll n, k;
+    cin >> n >> k;
+    vl a(n);
+    forn(i, n)
+    {
+        cin >> a[i];
+    }
+    ll baseSum = ((accumulate(all(a), 0ll) % mod) + mod) % mod;
+    ll maxSubarraySum = 0, currentSum = 0;
+    forn(i, n)
+    {
+        currentSum = max(a[i], currentSum + a[i]);
+        maxSubarraySum = max(maxSubarraySum, currentSum);
+    }
+    forn(i, k)
+    {
+        baseSum = (baseSum + maxSubarraySum) % mod;
+        maxSubarraySum = (maxSubarraySum * 2) % mod;
+    }
+    cout << baseSum << "\n";
+
+    //! Performing Binary exponentiation would fail because ans is not equal to 2^k * maxSubarraySum - maxSubarraySum + baseSum;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast_read();
+    int t;
+    cin >> t;
+
+    forn(i, t)
+    {
+        cerr << "\nTestcase " << i + 1 << " started\n";
+        solve();
+    }
+
+    return 0;
+}
---
+++ Competitive Programming/maths/1946A.cpp
@@ -0,0 +1,292 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cerr << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int> hmpi;
+typedef unordered_map<ll, ll> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int> hsi;
+typedef unordered_set<ll> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+#define sz(x) ((ll)(x).size())
+
+vpii dirs4 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+vpii dirs8 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cerr << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cerr << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cerr << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cerr << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+/*
+Read input test cases.Sort the array.Calculate the index of the current median.Iterate from the current median to the end of the array.For each element after the median, if it's less than or equal to the current median, increment it to make it greater than the current median.Count the number of operations required.Output the result.
+ */
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n + 1);
+    a.front() = INT_MIN;
+    forsn(i, 1, n + 1)
+    {
+        cin >> a[i];
+    }
+    sortall(a);
+    int idx = upper_bound(all(a), a[(n + 1) / 2]) - a.begin();
+    idx--;
+    int ans = 0;
+    cout << idx - (n + 1) / 2 + 1 << "\n";
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast_read();
+    int t;
+    cin >> t;
+
+    forn(i, t)
+    {
+        cerr << "\nTestcase " << i + 1 << " started\n";
+        solve();
+    }
+
+    return 0;
+}

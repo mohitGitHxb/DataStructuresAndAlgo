9c71a5ce55de87d892e2b9a95cc54b056c904b7d
mohitGitHxb <mohit2003second@gmail.com>
2024-05-07 08:37:51 +0530

    upsolving previous contests

---
+++ Contests Upsolving Leetcode and GFG/Math/Minimum cost  to equalize array.cpp
@@ -0,0 +1,579 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cerr << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define forv(it, a) for (auto it : a)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int> hmpi;
+typedef unordered_map<ll, ll> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int> hsi;
+typedef unordered_set<ll> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+#define sz(x) ((ll)(x).size())
+
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cerr << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cerr << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cerr << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cerr << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+/*
+ * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
+ */
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+class TrieNode
+{
+private:
+    struct Node
+    {
+        Node *links[26];
+        int cntEndWith = 0;
+        int cntPrefix = 0;
+
+        bool containsKey(char ch)
+        {
+            return (links[ch - 'a'] != nullptr);
+        }
+
+        Node *get(char ch)
+        {
+            return links[ch - 'a'];
+        }
+
+        void put(char ch, Node *node)
+        {
+            links[ch - 'a'] = node;
+        }
+
+        void increaseEnd()
+        {
+            cntEndWith++;
+        }
+
+        void increasePrefix()
+        {
+            cntPrefix++;
+        }
+
+        void deleteEnd()
+        {
+            cntEndWith--;
+        }
+
+        void reducePrefix()
+        {
+            cntPrefix--;
+        }
+
+        int getEnd()
+        {
+            return cntEndWith;
+        }
+
+        int getPrefix()
+        {
+            return cntPrefix;
+        }
+    };
+
+    Node *root;
+
+public:
+    TrieNode()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->increaseEnd();
+    }
+
+    int countWordsEqualTo(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+    int countWordsStartingWith(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getPrefix();
+    }
+
+    void erase(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+                node->reducePrefix();
+            }
+            else
+            {
+                return;
+            }
+        }
+        node->deleteEnd();
+    }
+};
+class DisjointSet
+{
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i < n + 1; i++)
+        {
+            parent.at(i) = i;
+            size.at(i) = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (rank.at(ultimateParentU) < rank.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+        }
+        else if (rank.at(ultimateParentV) < rank.at(ultimateParentU))
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+        }
+        else
+        {
+            parent.at(ultimateParentV) = parent.at(ultimateParentU);
+            rank.at(ultimateParentU)++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (size.at(ultimateParentU) < size.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+            size.at(ultimateParentV) += size.at(ultimateParentU);
+        }
+        else
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+            size.at(ultimateParentU) += size.at(ultimateParentV);
+        }
+    }
+};
+
+class Solution
+{
+private:
+    /*
+You are given an integer array nums and two integers cost1 and cost2. You are allowed to perform either of the following operations any number of times:
+
+    Choose an index i from nums and increase nums[i] by 1 for a cost of cost1.
+    Choose two different indices i, j, from nums and increase nums[i] and nums[j] by 1 for a cost of cost2.
+
+Return the minimum cost required to make all elements in the array equal.
+
+Since the answer may be very large, return it modulo 109 + 7.
+
+
+
+Example 1:
+
+Input: nums = [4,1], cost1 = 5, cost2 = 2
+
+Output: 15
+
+Explanation:
+
+The following operations can be performed to make the values equal:
+
+    Increase nums[1] by 1 for a cost of 5. nums becomes [4,2].
+    Increase nums[1] by 1 for a cost of 5. nums becomes [4,3].
+    Increase nums[1] by 1 for a cost of 5. nums becomes [4,4].
+
+The total cost is 15.
+
+Example 2:
+
+Input: nums = [2,3,3,3,5], cost1 = 2, cost2 = 1
+
+Output: 6
+
+Explanation:
+
+The following operations can be performed to make the values equal:
+
+    Increase nums[0] and nums[1] by 1 for a cost of 1. nums becomes [3,4,3,3,5].
+    Increase nums[0] and nums[2] by 1 for a cost of 1. nums becomes [4,4,4,3,5].
+    Increase nums[0] and nums[3] by 1 for a cost of 1. nums becomes [5,4,4,4,5].
+    Increase nums[1] and nums[2] by 1 for a cost of 1. nums becomes [5,5,5,4,5].
+    Increase nums[3] by 1 for a cost of 2. nums becomes [5,5,5,5,5].
+
+The total cost is 6.
+
+Example 3:
+
+Input: nums = [3,5,3], cost1 = 1, cost2 = 3
+
+Output: 4
+
+Explanation:
+
+The following operations can be performed to make the values equal:
+
+    Increase nums[0] by 1 for a cost of 1. nums becomes [4,5,3].
+    Increase nums[0] by 1 for a cost of 1. nums becomes [5,5,3].
+    Increase nums[2] by 1 for a cost of 1. nums becomes [5,5,4].
+    Increase nums[2] by 1 for a cost of 1. nums becomes [5,5,5].
+
+The total cost is 4.
+
+! THIS QUESTION IS A NIGHTMARE Either you know the code or you fail to solve !!
+     */
+public:
+    const int mod = 1e9 + 7;
+    Solution()
+    {
+        fast_read();
+    }
+    int minCostToEqualizeArray(vi &nums, int cost1, int cost2)
+    {
+        ll n = sz(nums);
+        ll mx = *max_element(all(nums));
+        ll mn = *min_element(all(nums));
+        ll sum = accumulate(all(nums), 0ll);
+        ll totalGap = mx * n - sum;
+        //* case 1
+        if (cost1 * 2 <= 2 || n <= 2)
+        {
+            return (totalGap * cost1) % mod;
+        }
+        //* case 2
+        ll op1 = max(0ll, (mx - mn) * 2 - totalGap);
+        ll op2 = totalGap - op1;
+        ll res = (op1 + op2 % 2) * cost1 + op2 / 2 * cost2;
+        //* case 3
+        totalGap += op1 / (n - 2) * n;
+        op1 %= n - 2;
+        op2 = totalGap - op1;
+        res = min(res, (op1 + op2 % 2) * cost1 + op2 / 2 * cost2);
+
+        //* case 4
+        forn(i, 2)
+        {
+            totalGap += n;
+            res = min(res, totalGap % 2 * cost1 + totalGap / 2 * cost2);
+        }
+
+        return res % mod;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution obj;
+
+    return 0;
+}
---
+++ Contests Upsolving Leetcode and GFG/Searching Sorting/find-the-integer-added-to-array-ii wc395.cpp
@@ -0,0 +1,543 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cerr << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define forv(it, a) for (auto it : a)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int> hmpi;
+typedef unordered_map<ll, ll> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int> hsi;
+typedef unordered_set<ll> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+#define sz(x) ((ll)(x).size())
+
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cerr << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cerr << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cerr << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cerr << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+/*
+ * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
+ */
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+class TrieNode
+{
+private:
+    struct Node
+    {
+        Node *links[26];
+        int cntEndWith = 0;
+        int cntPrefix = 0;
+
+        bool containsKey(char ch)
+        {
+            return (links[ch - 'a'] != nullptr);
+        }
+
+        Node *get(char ch)
+        {
+            return links[ch - 'a'];
+        }
+
+        void put(char ch, Node *node)
+        {
+            links[ch - 'a'] = node;
+        }
+
+        void increaseEnd()
+        {
+            cntEndWith++;
+        }
+
+        void increasePrefix()
+        {
+            cntPrefix++;
+        }
+
+        void deleteEnd()
+        {
+            cntEndWith--;
+        }
+
+        void reducePrefix()
+        {
+            cntPrefix--;
+        }
+
+        int getEnd()
+        {
+            return cntEndWith;
+        }
+
+        int getPrefix()
+        {
+            return cntPrefix;
+        }
+    };
+
+    Node *root;
+
+public:
+    TrieNode()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->increaseEnd();
+    }
+
+    int countWordsEqualTo(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+    int countWordsStartingWith(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getPrefix();
+    }
+
+    void erase(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+                node->reducePrefix();
+            }
+            else
+            {
+                return;
+            }
+        }
+        node->deleteEnd();
+    }
+};
+class DisjointSet
+{
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i < n + 1; i++)
+        {
+            parent.at(i) = i;
+            size.at(i) = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (rank.at(ultimateParentU) < rank.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+        }
+        else if (rank.at(ultimateParentV) < rank.at(ultimateParentU))
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+        }
+        else
+        {
+            parent.at(ultimateParentV) = parent.at(ultimateParentU);
+            rank.at(ultimateParentU)++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (size.at(ultimateParentU) < size.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+            size.at(ultimateParentV) += size.at(ultimateParentU);
+        }
+        else
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+            size.at(ultimateParentU) += size.at(ultimateParentV);
+        }
+    }
+};
+
+class Solution
+{
+private:
+    /*
+    You are given two integer arrays nums1 and nums2.
+
+From nums1 two elements have been removed, and all other elements have been increased (or decreased in the case of negative) by an integer, represented by the variable x.
+
+As a result, nums1 becomes equal to nums2. Two arrays are considered equal when they contain the same integers with the same frequencies.
+
+Return the minimum possible integer x that achieves this equivalence.
+
+
+
+Example 1:
+
+Input: nums1 = [4,20,16,12,8], nums2 = [14,18,10]
+
+Output: -2
+
+Explanation:
+
+After removing elements at indices [0,4] and adding -2, nums1 becomes [18,14,10].
+
+Example 2:
+
+Input: nums1 = [3,5,5,3], nums2 = [7,7]
+
+Output: 2
+
+Explanation:
+
+After removing elements at indices [0,3] and adding 2, nums1 becomes [7,7].
+
+
+     */
+public:
+    const int mod = 1e9 + 7;
+    Solution()
+    {
+        fast_read();
+    }
+    bool isPossible(vi &nums1, vi &nums2, int diff)
+    {
+        int neglect = 2;
+        for (int i = 0, j = 0; i < sz(nums1) and j < sz(nums2); i++)
+        {
+            if (diff == nums2[j] - nums1[i])
+            {
+                j++;
+            }
+            else
+            {
+                neglect--;
+                if (neglect < 0 or diff > nums2[j] - nums1[i])
+                    return false;
+            }
+        }
+        return neglect >= 0;
+    }
+    int minimumAddedInteger(vi &nums1, vi &nums2)
+    {
+        sortall(nums1);
+        sortall(nums2);
+        int minDiff = INT_MAX;
+        forn(i, sz(nums1))
+        {
+            int diff = nums2.front() - nums1[i];
+            // check if this diff is possible to achieve
+            if (isPossible(nums1, nums2, diff))
+            {
+                minDiff = min(minDiff, diff);
+            }
+        }
+        return (minDiff == INT_MAX ? 0 : minDiff);
+    }
+};

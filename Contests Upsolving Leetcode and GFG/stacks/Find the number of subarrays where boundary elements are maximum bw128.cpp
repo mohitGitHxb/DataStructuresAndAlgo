bf45d636380abb108bc5e71427a0ddcd0513426e
mohitGitHxb <mohit2003second@gmail.com>
2024-05-16 13:16:35 +0530

    Biweekly 128 done

---
+++ Contests Upsolving Leetcode and GFG/stacks/Find the number of subarrays where boundary elements are maximum bw128.cpp
@@ -0,0 +1,570 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define forv(it, a) for (auto it : a)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int> hmpi;
+typedef unordered_map<ll, ll> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int> hsi;
+typedef unordered_set<ll> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+#define sz(x) ((ll)(x).size())
+
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cout << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cout << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cout << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cout << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cout << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cout << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cout << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+/*
+ * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
+ */
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+class TrieNode
+{
+private:
+    struct Node
+    {
+        Node *links[26];
+        int cntEndWith = 0;
+        int cntPrefix = 0;
+
+        bool containsKey(char ch)
+        {
+            return (links[ch - 'a'] != nullptr);
+        }
+
+        Node *get(char ch)
+        {
+            return links[ch - 'a'];
+        }
+
+        void put(char ch, Node *node)
+        {
+            links[ch - 'a'] = node;
+        }
+
+        void increaseEnd()
+        {
+            cntEndWith++;
+        }
+
+        void increasePrefix()
+        {
+            cntPrefix++;
+        }
+
+        void deleteEnd()
+        {
+            cntEndWith--;
+        }
+
+        void reducePrefix()
+        {
+            cntPrefix--;
+        }
+
+        int getEnd()
+        {
+            return cntEndWith;
+        }
+
+        int getPrefix()
+        {
+            return cntPrefix;
+        }
+    };
+
+    Node *root;
+
+public:
+    TrieNode()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->increaseEnd();
+    }
+
+    int countWordsEqualTo(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+    int countWordsStartingWith(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getPrefix();
+    }
+
+    void erase(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+                node->reducePrefix();
+            }
+            else
+            {
+                return;
+            }
+        }
+        node->deleteEnd();
+    }
+};
+class DisjointSet
+{
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i < n + 1; i++)
+        {
+            parent.at(i) = i;
+            size.at(i) = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (rank.at(ultimateParentU) < rank.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+        }
+        else if (rank.at(ultimateParentV) < rank.at(ultimateParentU))
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+        }
+        else
+        {
+            parent.at(ultimateParentV) = parent.at(ultimateParentU);
+            rank.at(ultimateParentU)++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (size.at(ultimateParentU) < size.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+            size.at(ultimateParentV) += size.at(ultimateParentU);
+        }
+        else
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+            size.at(ultimateParentU) += size.at(ultimateParentV);
+        }
+    }
+};
+
+class Solution
+{
+private:
+    /*
+You are given an array of positive integers nums.
+
+Return the number of
+subarrays
+of nums, where the first and the last elements of the subarray are equal to the largest element in the subarray.
+
+
+
+Example 1:
+
+Input: nums = [1,4,3,3,2]
+
+Output: 6
+
+Explanation:
+
+There are 6 subarrays which have the first and the last elements equal to the largest element of the subarray:
+
+    subarray [1,4,3,3,2], with its largest element 1. The first element is 1 and the last element is also 1.
+    subarray [1,4,3,3,2], with its largest element 4. The first element is 4 and the last element is also 4.
+    subarray [1,4,3,3,2], with its largest element 3. The first element is 3 and the last element is also 3.
+    subarray [1,4,3,3,2], with its largest element 3. The first element is 3 and the last element is also 3.
+    subarray [1,4,3,3,2], with its largest element 2. The first element is 2 and the last element is also 2.
+    subarray [1,4,3,3,2], with its largest element 3. The first element is 3 and the last element is also 3.
+
+Hence, we return 6.
+
+
+     */
+public:
+    const int mod = 1e9 + 7;
+    Solution()
+    {
+        fast_read();
+    }
+    /*
+    @ Intuiton part or observation
+    & Breaking down the problem in two parts
+    &1) since we need to find the subarrays whose first and last element is same and also equal to the maximum element of the subarray we can divide this problem in two segments
+
+    % To find the subarrays ending and starting with same elemenet we can maintain a map like data structure to store the number of occurences of the element in previous side and just by looking at the size of the map we can determine the subarray count
+
+    % Since we also want to have the ending and starting element equal to the maximum element we need to make sure that no element greater than current ith element exists between any two indices
+
+    & For eg. _ _ 999999 _3 _ _ 4 3 _ _ 3 _ 3
+    here we  can't form array with first 3 as there is a 4 that is after it
+
+     */
+    ll numberOfSubarrays(vi &nums)
+    {
+        stack<int> stk;
+        vi previousGreator(sz(nums), -1);
+        forn(i, sz(nums))
+        {
+            while (!stk.empty() and nums[stk.top()] <= nums[i])
+            {
+                stk.pop();
+            }
+            if (!stk.empty())
+            {
+                previousGreator[i] = stk.top();
+            }
+            stk.push(i);
+        }
+        ll subarrays = 0;
+        // customPrint(previousGreator);
+        unordered_map<int, vi> occurrences;
+        forn(i, sz(nums))
+        {
+            occurrences[nums[i]].pb(i);
+            if (!occurrences.count(nums[i]))
+            {
+                subarrays++;
+            }
+            else
+            {
+                int largestNearestElement = previousGreator[i];
+                if (largestNearestElement == -1)
+                {
+                    subarrays += occurrences[nums[i]].size();
+                }
+                else
+                {
+                    auto it = upper_bound(all(occurrences[nums[i]]), largestNearestElement);
+                    int idx = it - occurrences[nums[i]].begin();
+                    int elementToSkip = occurrences[nums[i]].size() - idx;
+                    subarrays += elementToSkip;
+                }
+            }
+            // customPrint(occurrences[nums[i]]);
+        }
+        return subarrays;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

5e9f8d8b076131b21ac1e7ead10b8438e43ed983
mohitGitHxb <mohit2003second@gmail.com>
2024-05-24 14:17:36 +0530

    backlog.clear();

---
+++ Contests Upsolving Leetcode and GFG/Arrays/Maximum difference score in a grid bw397.cpp
@@ -0,0 +1,550 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
+ */
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define forv(it, a) for (auto it : a)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int, custom_hash> hmpi;
+typedef unordered_map<ll, ll, custom_hash> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int, custom_hash> hsi;
+typedef unordered_set<ll, custom_hash> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+#define sz(x) ((ll)(x).size())
+
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cout << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cout << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cout << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cout << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cout << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cout << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cout << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+class TrieNode
+{
+private:
+    struct Node
+    {
+        Node *links[26];
+        int cntEndWith = 0;
+        int cntPrefix = 0;
+
+        bool containsKey(char ch)
+        {
+            return (links[ch - 'a'] != nullptr);
+        }
+
+        Node *get(char ch)
+        {
+            return links[ch - 'a'];
+        }
+
+        void put(char ch, Node *node)
+        {
+            links[ch - 'a'] = node;
+        }
+
+        void increaseEnd()
+        {
+            cntEndWith++;
+        }
+
+        void increasePrefix()
+        {
+            cntPrefix++;
+        }
+
+        void deleteEnd()
+        {
+            cntEndWith--;
+        }
+
+        void reducePrefix()
+        {
+            cntPrefix--;
+        }
+
+        int getEnd()
+        {
+            return cntEndWith;
+        }
+
+        int getPrefix()
+        {
+            return cntPrefix;
+        }
+    };
+
+    Node *root;
+
+public:
+    TrieNode()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->increaseEnd();
+    }
+
+    int countWordsEqualTo(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+    int countWordsStartingWith(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getPrefix();
+    }
+
+    void erase(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+                node->reducePrefix();
+            }
+            else
+            {
+                return;
+            }
+        }
+        node->deleteEnd();
+    }
+};
+class DisjointSet
+{
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i < n + 1; i++)
+        {
+            parent.at(i) = i;
+            size.at(i) = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (rank.at(ultimateParentU) < rank.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+        }
+        else if (rank.at(ultimateParentV) < rank.at(ultimateParentU))
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+        }
+        else
+        {
+            parent.at(ultimateParentV) = parent.at(ultimateParentU);
+            rank.at(ultimateParentU)++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (size.at(ultimateParentU) < size.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+            size.at(ultimateParentV) += size.at(ultimateParentU);
+        }
+        else
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+            size.at(ultimateParentU) += size.at(ultimateParentV);
+        }
+    }
+};
+
+class Solution
+{
+private:
+    /*
+        You are given an m x n matrix grid consisting of positive integers. You can move from a cell in the matrix to any other cell that is either to the bottom or to the right (not necessarily adjacent). The score of a move from a cell with the value c1 to a cell with the value c2 is c2 - c1.
+
+You can start at any cell, and you have to make at least one move.
+
+Return the maximum total score you can achieve.
+
+
+
+Example 1:
+
+Input: grid = [[9,5,7,3],[8,9,6,1],[6,7,14,3],[2,5,3,1]]
+
+Output: 9
+
+Explanation: We start at the cell (0, 1), and we perform the following moves:
+- Move from the cell (0, 1) to (2, 1) with a score of 7 - 5 = 2.
+- Move from the cell (2, 1) to (2, 2) with a score of 14 - 7 = 7.
+The total score is 2 + 7 = 9.
+
+     */
+public:
+    const int mod = 1e9 + 7;
+    Solution()
+    {
+        fast_read();
+    }
+    int maxScore(vvi &grid)
+    {
+        vvi suffixBottomMaximums(sz(grid), vi(sz(grid[0]), -1e9));
+        vvi suffixRightMaximums(sz(grid), vi(sz(grid[0]), -1e9));
+        forn(col, sz(grid[0]))
+        {
+            rforn(row, sz(grid) - 2)
+            {
+                suffixBottomMaximums[row][col] = max(suffixBottomMaximums[row + 1][col], grid[row + 1][col]);
+            }
+        }
+        // customPrint(suffixBottomMaximums);
+        forn(row, sz(grid))
+        {
+            rforn(col, sz(grid[0]) - 2)
+            {
+                suffixRightMaximums[row][col] = max(suffixRightMaximums[row][col + 1], grid[row][col + 1]);
+            }
+        }
+        // customPrint(suffixRightMaximums);
+        vvi suffixCombinedMaximums(sz(grid), vi(sz(grid[0]), -1e9));
+        suffixCombinedMaximums.back() = suffixRightMaximums.back();
+        forn(row, sz(grid))
+        {
+            suffixCombinedMaximums[row][sz(grid[0]) - 1] = suffixBottomMaximums[row][sz(grid[0]) - 1];
+        }
+        rforn(row, sz(grid) - 2)
+        {
+            rforn(col, sz(grid[0]) - 2)
+            {
+                suffixCombinedMaximums[row][col] = max({suffixCombinedMaximums[row + 1][col], suffixCombinedMaximums[row][col + 1], suffixCombinedMaximums[row + 1][col + 1], suffixBottomMaximums[row][col], suffixRightMaximums[row][col]});
+            }
+        }
+        // customPrint(suffixCombinedMaximums);
+        int ans = INT_MIN;
+        forn(row, sz(grid))
+        {
+            forn(col, sz(grid[0]))
+            {
+                if (row == sz(grid) - 1 && col == sz(grid[0]) - 1)
+                    continue;
+                ans = max(ans, -grid[row][col] + suffixCombinedMaximums[row][col]);
+                // if (col + 1 < sz(grid[0]) && row + 1 < sz(grid))
+                // {
+                //     ans = max(ans, grid[row][col] - max(suffixBottomMaximums[row][col + 1], suffixRightMaximums[row + 1][col]));
+                // }
+            }
+        }
+        return ans;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution obj;
+    cout << "\n\n\tProgram ended\n\n";
+    return 0;
+}

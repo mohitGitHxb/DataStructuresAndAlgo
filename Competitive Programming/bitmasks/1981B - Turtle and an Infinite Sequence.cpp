2969c73750bd7c35e34fa7be6d4eb5fa5c516a66
mohitGitHxb <mohit2003second@gmail.com>
2024-06-11 20:31:43 +0530

    Added codes

---
+++ Competitive Programming/bitmasks/1981B - Turtle and an Infinite Sequence.cpp
@@ -0,0 +1,369 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
+ */
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#pragma GCC optimize("Ofast", "unroll-loops", "omit-frame-pointer", "inline")
+#pragma GCC target("avx,avx2,fma")
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define forv(it, a) for (auto it : a)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int, custom_hash> hmpi;
+typedef unordered_map<ll, ll, custom_hash> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int, custom_hash> hsi;
+typedef unordered_set<ll, custom_hash> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+#define sz(x) ((ll)(x).size())
+
+vpii dirs4 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+vpii dirs8 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cerr << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+template <class T = int>
+void customPrint(priority_queue<T, vector<T>, greater<T>> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cerr << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cerr << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cerr << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+/*
+There is a sequence a0,a1,a2,… of infinite length. Initially ai=i for every non-negative integer i
+
+.
+
+After every second, each element of the sequence will simultaneously change. ai
+will change to ai−1∣ai∣ai+1 for every positive integer i. a0 will change to a0∣a1. Here, |
+
+denotes bitwise OR.
+
+Turtle is asked to find the value of an
+after m seconds. In particular, if m=0, then he needs to find the initial value of an. He is tired of calculating so many values, so please help him!
+ */
+
+void solve()
+{
+
+    int n, m;
+    cin >> n >> m;
+    ll OR = 0;
+    // forsn(i, max(n - m, 0), n + m + 1)
+    // {
+    //     OR |= i;
+    // } // (n-m) to (n+m) this will throw TLE
+
+    ll lower = max(n - m, 0);
+    ll upper = n + m;
+
+    //* O(log(n+m))
+    // & Compute OR in log(n+m) time
+    /*
+    % first bit follows this pattern 010101010101
+    % second bit follows this pattern 001100110011
+    % third bit follows this pattern 000011110000
+    % fourth bit follows this pattern 000000001111
+
+    ^ we observed that ith bit will have 2^i 0 then 2^i ones
+    ^ so to compute or effeciently we need to check any one bit in the ith bit column is set to 1
+    % now we can divide this pattern 0000111100001111 into blocks 0000 1st block then next set of 0000 second block
+    % block size = 2^i
+    & Now to have a bit set in the entire column L and R should belong to different blocks (e.g : L:0000 11 R:11 0000 1111 )
+
+     */
+    forn(i, 32)
+    {
+        int c1 = upper / (1LL << i);
+        if (c1 & 1)
+        {
+            OR |= (1LL << i);
+        }
+        else if (lower == 0)
+        {
+            if (c1)
+                OR |= (1LL << i);
+        }
+        int c2 = lower / (1LL << i);
+        if (c1 != c2)
+        {
+            OR |= (1LL << i);
+        }
+    }
+
+    cout << OR << "\n";
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast_read();
+    int t;
+    cin >> t;
+
+    forn(i, t)
+    {
+        cerr << "\nTestcase " << i + 1 << " started\n";
+        solve();
+    }
+
+    return 0;
+}

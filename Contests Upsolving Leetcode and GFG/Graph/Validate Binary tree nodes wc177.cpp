e98c39512b39691ec4deabc93ec85189e09bf22b
mohitGitHxb <mohit2003second@gmail.com>
2024-06-20 14:16:47 +0530

    Added questiosn

---
+++ Contests Upsolving Leetcode and GFG/Graph/Validate Binary tree nodes wc177.cpp
@@ -0,0 +1,694 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+/*
+ * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
+ */
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#pragma GCC optimize("Ofast", "unroll-loops", "omit-frame-pointer", "inline")
+#pragma GCC target("avx,avx2,fma")
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(), v.rend()
+#define clr(x) memset(x, false, sizeof(x))
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define SUM(v) accumulate(all(v), 0LL)
+#define endl "\n"
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define forv(it, a) for (auto &it : a)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int, custom_hash> hmpii;
+typedef unordered_map<ll, ll, custom_hash> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int, custom_hash> hsi;
+typedef unordered_set<ll, custom_hash> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+template <class T = pii>
+using oset =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T = pii>
+using mset = tree<T, null_type, less_equal<T>, rb_tree_tag,
+                  tree_order_statistics_node_update>;
+#define sz(x) ((ll)(x).size())
+
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cout << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cout << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cout << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const pair<T, U> &p)
+{
+    cout << "{" << customPrint(p.first) << ":";
+    customPrint(p.second);
+    cout << "}\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cout << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cout << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cout << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cout << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        cout << "{" << i.first << ":";
+        customPrint(i.second);
+        cout << "}\n";
+    }
+    cout << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cout << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cout << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+
+class TrieNode
+{
+private:
+    struct Node
+    {
+        Node *links[26];
+        int cntEndWith = 0;
+        int cntPrefix = 0;
+
+        bool containsKey(char ch)
+        {
+            return (links[ch - 'a'] != nullptr);
+        }
+
+        Node *get(char ch)
+        {
+            return links[ch - 'a'];
+        }
+
+        void put(char ch, Node *node)
+        {
+            links[ch - 'a'] = node;
+        }
+
+        void increaseEnd()
+        {
+            cntEndWith++;
+        }
+
+        void increasePrefix()
+        {
+            cntPrefix++;
+        }
+
+        void deleteEnd()
+        {
+            cntEndWith--;
+        }
+
+        void reducePrefix()
+        {
+            cntPrefix--;
+        }
+
+        int getEnd()
+        {
+            return cntEndWith;
+        }
+
+        int getPrefix()
+        {
+            return cntPrefix;
+        }
+    };
+
+    Node *root;
+
+public:
+    TrieNode()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->increaseEnd();
+    }
+
+    int countWordsEqualTo(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+    int countWordsStartingWith(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getPrefix();
+    }
+
+    void erase(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+                node->reducePrefix();
+            }
+            else
+            {
+                return;
+            }
+        }
+        node->deleteEnd();
+    }
+};
+
+class Fenwick
+{
+private:
+    vector<long long> bit, a;
+    long long n;
+
+public:
+    Fenwick(long long n)
+    {
+        this->n = n;
+        bit.resize(n + 1);
+    }
+    Fenwick(vector<long long> &arr)
+    {
+        n = arr.size();
+        bit.resize(n + 1);
+        this->a = arr;
+        build(a);
+    }
+    void build(vector<long long> &arr)
+    {
+        for (long long i = 0; i < n; i++)
+        {
+            update(i + 1, arr.at(i));
+        }
+    }
+    long long query(long long idx)
+    {
+        long long sum = 0;
+        while (idx > 0)
+        {
+            sum += bit.at(idx);
+            idx -= idx & (-idx); // sets the last set bit to 0
+        }
+        return sum;
+    }
+    void update(long long idx, long long val)
+    {
+        while (idx <= n)
+        {
+            bit.at(idx) += val;
+            idx += idx & (-idx); // sets the last unset bit to 1
+        }
+    }
+    void replace(long long idx, long long val)
+    {
+        update(idx + 1, -a.at(idx) + val);
+        a.at(idx) = val;
+    }
+    void printBitVector()
+    {
+        for (long long i = 1; i <= n; i++)
+        {
+            cout << bit.at(i) << " ";
+        }
+        cout << "\n";
+    }
+    void rangeUpdate(long long l, long long r, long long val)
+    {
+        update(l, val);
+        update(r + 1, -val);
+    }
+};
+
+class DisjointSet
+{
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i < n + 1; i++)
+        {
+            parent.at(i) = i;
+            size.at(i) = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (rank.at(ultimateParentU) < rank.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+        }
+        else if (rank.at(ultimateParentV) < rank.at(ultimateParentU))
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+        }
+        else
+        {
+            parent.at(ultimateParentV) = parent.at(ultimateParentU);
+            rank.at(ultimateParentU)++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ultimateParentU = findUPar(u);
+        int ultimateParentV = findUPar(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+        if (size.at(ultimateParentU) < size.at(ultimateParentV))
+        {
+            parent.at(ultimateParentU) = ultimateParentV;
+            size.at(ultimateParentV) += size.at(ultimateParentU);
+        }
+        else
+        {
+            parent.at(ultimateParentV) = ultimateParentU;
+            size.at(ultimateParentU) += size.at(ultimateParentV);
+        }
+    }
+};
+
+class Solution
+{
+private:
+    /*
+You have n binary tree nodes numbered from 0 to n - 1 where node i has two children leftChild[i] and rightChild[i], return true if and only if all the given nodes form exactly one valid binary tree.
+
+If node i has no left child then leftChild[i] will equal -1, similarly for the right child.
+
+Note that the nodes have no values and that we only use the node numbers in this problem.
+
+
+
+Example 1:
+
+Input: n = 4, leftChild = [1,-1,3,-1], rightChild = [2,-1,-1,-1]
+Output: true
+
+Example 2:
+
+Input: n = 4, leftChild = [1,-1,3,-1], rightChild = [2,3,-1,-1]
+Output: false
+
+Example 3:
+
+Input: n = 2, leftChild = [1,0], rightChild = [-1,-1]
+Output: false
+
+@ Approach : 
+& We know the following properties of the trees are
+% It only has one root node
+% it shouldn't have any cycles
+% A single node can have atmost one 1 parent
+% Number of edges = number of nodes - 1
+
+& Now we need to check for these properties in the given tree
+& So first we build the graph and in order to find the root and detect cycle which algorithm works fine?
+& It's a topological sort which gives us the linear ordering of the nodes which will be helpful to determine which node is the root node
+& Also while building our topological sort array we can also check if indegree of any node is more than 1 or not
+
+& Now we can check whether the tree is a disconnected component or having multiple roots
+
+^ using topological sort with normal graph traversal algorithm like bfs or dfs can determine whether the given tree is a valid binary tree
+
+? Note that we won't need to check for the condition of number of edges as it is being taken care of by dfs and indegree exceeding 1
+
+     */
+public:
+    const int mod = 1e9 + 7;
+    Solution()
+    {
+        fast_read();
+    }
+    vi topoSort(vvi &adj)
+    {
+        int n = adj.size();
+        vi indegree(n);
+        forn(i, n)
+        {
+            for (int j : adj[i])
+            {
+                indegree[j]++;
+                if (indegree[j] > 1)
+                {
+                    return {};
+                }
+            }
+        }
+        vi topo;
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.pb(node);
+            for (int it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        return topo;
+    }
+    void dfs(int src, vvi &adj, vector<bool> &vis)
+    {
+        vis[src] = true;
+        for (auto it : adj[src])
+        {
+            if (!vis[it])
+                dfs(it, adj, vis);
+        }
+    }
+    bool validateBinaryTreeNodes(int n, vi &leftChild, vi &rightChild)
+    {
+        vvi adj(n);
+        forn(i, n)
+        {
+            if (leftChild[i] != -1)
+            {
+                adj[i].push_back(leftChild[i]);
+            }
+            if (rightChild[i] != -1)
+            {
+                adj[i].push_back(rightChild[i]);
+            }
+        }
+        vi topologicalOrder = topoSort(adj);
+        if (topologicalOrder.size() != n)
+            return false;
+        int countOfRoots = 0;
+        vector<bool> vis(n);
+        forn(i, n)
+        {
+            if (!vis[topologicalOrder[i]])
+            {
+                dfs(topologicalOrder[i], adj, vis);
+                countOfRoots++;
+            }
+        }
+        return countOfRoots == 1;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution obj;
+    vi leftChild = {3, -1, 1, -1};
+    vi rightChild = {-1, -1, 0, -1};
+    cout << obj.validateBinaryTreeNodes(4, leftChild, rightChild);
+    cout << "\n\n\tProgram ended\n\n";
+    return 0;
+}

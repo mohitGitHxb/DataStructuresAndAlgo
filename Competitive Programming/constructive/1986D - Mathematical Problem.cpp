b03a7302dcd25da533e1f4c081e6955033a58f12
mohitGitHxb <mohit2003second@gmail.com>
2024-06-24 13:42:04 +0530

    Added problems

---
+++ Competitive Programming/constructive/1986D - Mathematical Problem.cpp
@@ -0,0 +1,414 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+/*
+ * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
+ */
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#pragma GCC optimize("Ofast", "unroll-loops", "omit-frame-pointer", "inline")
+#pragma GCC target("avx,avx2,fma")
+#define gc getchar_unlocked
+#define ll long long
+#define deb(x) cerr << #x << "=" << x << endl
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(), v.rend()
+#define clr(x) memset(x, false, sizeof(x))
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define SUM(v) accumulate(all(v), 0LL)
+#define endl "\n"
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define forv(it, a) for (auto &it : a)
+#define PI 3.1415926535897932384626
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define sz(x) ((ll)(x).size())
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<string> vsr;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef unordered_map<int, int, custom_hash> hmpi;
+typedef unordered_map<ll, ll, custom_hash> hmpll;
+typedef unordered_map<int, pii> hmppi;
+typedef unordered_map<int, vi> hmpvi;
+typedef unordered_set<int, custom_hash> hsi;
+typedef unordered_set<ll, custom_hash> hsll;
+typedef unordered_set<pii> hspi;
+typedef unordered_set<pl> hspl;
+typedef map<int, int> mpi;
+typedef map<ll, ll> mpll;
+typedef map<int, pii> mppi;
+typedef map<int, vi> mpvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<pii> spi;
+typedef set<pl> spl;
+template <class T = pii>
+using oset =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T = pii>
+using mset = tree<T, null_type, less_equal<T>, rb_tree_tag,
+                  tree_order_statistics_node_update>;
+
+vpii dirs4 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+vpii dirs8 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+void fast_read()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+template <class T = int>
+void customPrint(const T &t)
+{
+    cerr << t << " ";
+}
+
+template <class T = int>
+void customPrint(const vector<T> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const vector<vector<T>> &vec)
+{
+    cerr << "[";
+    for (auto &&i : vec)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_map<T, U, custom_hash> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const map<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const set<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_set<T, custom_hash> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(priority_queue<T> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+template <class T = int>
+void customPrint(priority_queue<T, vector<T>, greater<T>> pq)
+{
+    cerr << "[";
+    while (!pq.empty())
+    {
+        customPrint(pq.top());
+        pq.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(stack<T> st)
+{
+    cerr << "[";
+    while (!st.empty())
+    {
+        customPrint(st.top());
+        st.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(queue<T> q)
+{
+    cerr << "[";
+    while (!q.empty())
+    {
+        customPrint(q.front());
+        q.pop();
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(deque<T> dq)
+{
+    cerr << "[";
+    for (auto &i : dq)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+
+template <class T = int, class U = int>
+void customPrint(const unordered_multimap<T, U> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        cerr << "{" << i.first << ":";
+        customPrint(i.second);
+        cerr << "}\n";
+    }
+    cerr << "]\n";
+}
+
+template <class T = int>
+void customPrint(const unordered_multiset<T> &mp)
+{
+    cerr << "[";
+    for (auto &i : mp)
+    {
+        customPrint(i);
+    }
+    cerr << "]\n";
+}
+void printBitRepresentation(long long n)
+{
+    string binary = bitset<64>(n).to_string(); // converts to binary
+    cout << "Binary representation: " << binary << "\n";
+}
+bool isPrime_sqrt(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+long long binaryExponentiation(long long base, long long exponent, long long mod)
+{
+    long long res = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return res;
+}
+long long getModularMultiplicativeInverse(long long num, long long mod)
+{
+    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
+    if (__gcd(num, mod) != 1)
+    {
+        return -1;
+    }
+    return binaryExponentiation(num, mod - 2, mod);
+}
+/*
+
+You are given a string s
+of length n>1, consisting of digits from 0 to 9. You must insert exactly n−2 symbols + (addition) or ×
+
+(multiplication) into this string to form a valid arithmetic expression.
+
+In this problem, the symbols cannot be placed before the first or after the last character of the string s
+, and two symbols cannot be written consecutively. Also, note that the order of the digits in the string cannot be changed. Let's consider s=987009
+
+:
+
+    From this string, the following arithmetic expressions can be obtained: 9×8+70×0+9=81
+
+, 98×7×0+0×9=0, 9+8+7+0+09=9+8+7+0+9=33. Note that the number 09 is considered valid and is simply transformed into 9
+.
+From this string, the following arithmetic expressions cannot be obtained: +9×8×70+09
+(symbols should only be placed between digits), 98×70+0+9 (since there are 6 digits, there must be exactly 4
+
+    symbols).
+
+The result of the arithmetic expression is calculated according to the rules of mathematics — first all multiplication operations are performed, then addition. You need to find the minimum result that can be obtained by inserting exactly n−2
+addition or multiplication symbols into the given string s.
+
+ */
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    // & if string size is <= 2 then just return the stoi(string)
+    if(n <= 2){
+        cout << stoi(s) << "\n";
+        return;
+    }
+
+    /*
+    % prepares all combinations of selecting 2 digits from the string
+    % for example: string s = 987009
+    [[98 7 0 0 9 ]
+    [9 87 0 0 9 ]
+    [9 8 70 0 9 ]
+    [9 8 7 0 9 ]
+    [9 8 7 0 9 ]
+    ]
+
+     */
+    vvi allCombos;
+    forn(i, n - 1)
+    {
+        vi currCombo;
+
+        forn(j, n)
+        {
+            if (j == i)
+            {
+                currCombo.pb(stoi(s.substr(j, 2)));
+                j++;
+            }
+            else
+            {
+                currCombo.pb(stoi(s.substr(j, 1)));
+            }
+        }
+        allCombos.eb(currCombo);
+    }
+    // customPrint(allCombos);
+    /*
+        @ Observations:
+        & If any combo contains a zero then answer is 0
+        & otherwise it is always beneficial to add instead of multiply to get minimum result
+        & In case of 1s remember 1 * x < 1 + x
+        & To take care of this we can simply subtract occurrence of 1 from the sum(combo);
+
+        ^ Take out minimum of all combos
+     */
+    ll minimum = LONG_MAX;
+    forv(combo, allCombos)
+    {
+        bool containsZero = count(all(combo), 0) > 0;
+        if (containsZero)
+        {
+            cout << 0 << "\n";
+            return;
+        }
+        ll sum = SUM(combo);
+        ll occurenceOf1 = count(all(combo), 1);
+        sum -= occurenceOf1;
+        //& in case of 101 or 1011 sum == occurrence of 1 for some combos
+        minimum = min(minimum, max(1ll,sum));
+    }
+
+    cout << minimum << "\n";
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast_read();
+    int t;
+    cin >> t;
+    forn(i, t)
+    {
+        cerr << "\nTestcase " << i + 1 << " started\n";
+        solve();
+    }
+
+    return 0;
+}

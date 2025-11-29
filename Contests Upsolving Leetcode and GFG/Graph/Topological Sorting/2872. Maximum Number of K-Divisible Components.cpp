a67ad9e853daa02bd928e95e7bc8ddcb27e5c255
mohitGitHxb <mohit2003second@gmail.com>
2025-11-29 13:41:19 +0530

    update: better and improved code snippet

--- Contests Upsolving Leetcode and GFG/Graph/Topological Sorting/2872. Maximum Number of K-Divisible Components.cpp
+++ Contests Upsolving Leetcode and GFG/Graph/Topological Sorting/2872. Maximum Number of K-Divisible Components.cpp
@@ -1,383 +1,155 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-/*
- * CUSTOM hash function which avoids collisions and hence makes unordered maps / sets faster
- */
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-#pragma GCC optimize("O3")
-#pragma GCC optimize("Ofast", "inline", "unroll-loops", "no-stack-protector")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
-#define gc getchar_unlocked
-#define ull unsigned long long
-#define ll long long
-#define deb(x) cout << #x << "=" << x << endl
-#define pb push_back
-#define eb emplace_back
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define rall(v) v.rbegin(), v.rend()
-#define clr(x) memset(x, false, sizeof(x))
-#define MIN(v) *min_element(all(v))
-#define MAX(v) *max_element(all(v))
-#define SUM(v) accumulate(all(v), 0LL)
-#define endl "\n"
-#define sortall(x) sort(all(x))
-#define forv(it, a) for (auto &it : a)
-#define PI 3.1415926535897932384626
-ll MOD = 998244353;
-double eps = 1e-12;
-#define forsn(i, s, e) for (ll i = s; i < e; i++)
-#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<string> vsr;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef vector<vvl> vvvl;
-template <class T = pii>
-using oset =
-    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <class T = pii>
-using mset = tree<T, null_type, less_equal<T>, rb_tree_tag,
-                  tree_order_statistics_node_update>;
-#define sz(x) ((ll)(x).size())
-
-static auto init = []
-{
-    ios_base::sync_with_stdio(false);
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    return 0;
-}();
-template <class T = int>
-void customPrint(const T &t)
-{
-    cout << t << " ";
-}
-
-template <class T = int>
-void customPrint(const vector<T> &vec)
-{
-    cout << "[";
-    for (auto &&i : vec)
-    {
-        customPrint(i);
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(const vector<vector<T>> &vec)
-{
-    cout << "[";
-    for (auto &&i : vec)
-    {
-        customPrint(i);
-    }
-    cout << "]\n";
-}
-
-template <class T = int, class U = int>
-void customPrint(const pair<T, U> &p)
-{
-    cout << "{" << customPrint(p.first) << ":";
-    customPrint(p.second);
-    cout << "}\n";
-}
-
-template <class T = int, class U = int>
-void customPrint(const unordered_map<T, U, custom_hash> &mp)
-{
-    cout << "[";
-    for (auto &i : mp)
-    {
-        cout << "{" << i.first << ":";
-        customPrint(i.second);
-        cout << "}\n";
-    }
-    cout << "]\n";
-}
-
-template <class T = int, class U = int>
-void customPrint(const map<T, U> &mp)
-{
-    cout << "[";
-    for (auto &i : mp)
-    {
-        cout << "{" << i.first << ":";
-        customPrint(i.second);
-        cout << "}\n";
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(const multiset<T> &mp)
-{
-    cout << "[";
-    for (auto &i : mp)
-    {
-        customPrint(i);
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(const set<T> &mp)
-{
-    cout << "[";
-    for (auto &i : mp)
-    {
-        customPrint(i);
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(const unordered_set<T, custom_hash> &mp)
-{
-    cout << "[";
-    for (auto &i : mp)
-    {
-        customPrint(i);
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(priority_queue<T> pq)
-{
-    cout << "[";
-    while (!pq.empty())
-    {
-        customPrint(pq.top());
-        pq.pop();
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(stack<T> st)
-{
-    cout << "[";
-    while (!st.empty())
-    {
-        customPrint(st.top());
-        st.pop();
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(queue<T> q)
-{
-    cout << "[";
-    while (!q.empty())
-    {
-        customPrint(q.front());
-        q.pop();
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(deque<T> dq)
-{
-    cout << "[";
-    for (auto &i : dq)
-    {
-        customPrint(i);
-    }
-    cout << "]\n";
-}
-
-template <class T = int, class U = int>
-void customPrint(const unordered_multimap<T, U> &mp)
-{
-    cout << "[";
-    for (auto &i : mp)
-    {
-        cout << "{" << i.first << ":";
-        customPrint(i.second);
-        cout << "}\n";
-    }
-    cout << "]\n";
-}
-
-template <class T = int>
-void customPrint(const unordered_multiset<T> &mp)
-{
-    cout << "[";
-    for (auto &i : mp)
-    {
-        customPrint(i);
-    }
-    cout << "]\n";
-}
-void printBitRepresentation(long long n)
-{
-    string binary = bitset<64>(n).to_string(); // converts to binary
-    cout << "Binary representation: " << binary << "\n";
-}
-bool isPrime_sqrt(ll n)
-{
-    if (n == 1)
-        return false;
-    for (ll i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
-long long binaryExponentiation(long long base, long long exponent, long long mod)
-{
-    long long res = 1;
-    while (exponent > 0)
-    {
-        if (exponent % 2 == 1)
-        {
-            res = (res * base) % mod;
-        }
-        base = (base * base) % mod;
-        exponent >>= 1;
-    }
-    return res;
-}
-long long getModularMultiplicativeInverse(long long num, long long mod)
-{
-    // multiplicative inverse only exists if num and mod are relatively prime (coprime) i.e gcd(num, mod) = 1
-    if (__gcd(num, mod) != 1)
-    {
-        return -1;
-    }
-    return binaryExponentiation(num, mod - 2, mod);
-}
-
 class Solution
 {
 private:
     /*
+    Problem statement:
+    There is an undirected tree with n nodes labeled from 0 to n - 1. You are given the integer n and a 2D integer array edges of length n - 1, where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
 
-        There is an undirected tree with n nodes labeled from 0 to n - 1. You are given the integer n and a 2D integer array edges of length n - 1, where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
+    You are also given a 0-indexed integer array values of length n, where values[i] is the value associated with the ith node, and an integer k.
 
-        You are also given a 0-indexed integer array values of length n, where values[i] is the value associated with the ith node, and an integer k.
+    A valid split of the tree is obtained by removing any set of edges, possibly empty, from the tree such that the resulting components all have values that are divisible by k, where the value of a connected component is the sum of the values of its nodes.
 
-        A valid split of the tree is obtained by removing any set of edges, possibly empty, from the tree such that the resulting components all have values that are divisible by k, where the value of a connected component is the sum of the values of its nodes.
-
-        Return the maximum number of components in any valid split.
-
-
-
-        Example 1:
-
-        Input: n = 5, edges = [[0,2],[1,2],[1,3],[2,4]], values = [1,8,1,4,4], k = 6
-        Output: 2
-        Explanation: We remove the edge connecting node 1 with 2. The resulting split is valid because:
-        - The value of the component containing nodes 1 and 3 is values[1] + values[3] = 12.
-        - The value of the component containing nodes 0, 2, and 4 is values[0] + values[2] + values[4] = 6.
-        It can be shown that no other valid split has more than 2 connected components.
-
-        @ approach
-        ! wrong approach [complex]
-        &- Topological sort to find the root of the tree and then calculating subtree sums of each subtree.
-        &- Then running dfs on the tree to check for a optimal breaking point
-
-        * Correct approach #1
-        &- Think in terms of nodes not the edges.
-        &- For a leaf node we can have two choices either it is divisible by k which means it can be separated from its parent or it is not divisible by k which means it can be merged with its parent.
-        &- Keep doing this for all the new leaf nodes formed
-        &- For edge removal we can use indegrees;
-
-        &- Process would look like this: Take a leaf node and check if it is divisible by k or not.
-        &- If it is then increase component and reduce indegrees of all its neighbours.
-        &- If it is not divisible by k then add it to the parent and reduce indegrees of all its neighbours.
-
-        * Time complexity: O(n)
-        * Space complexity: O(n)
+    Return the maximum number of components in any valid split.
 
 
+     * PROBLEM UNDERSTANDING:
+     * - Given an undirected tree with n nodes (0 to n-1)
+     * - Each node has a value, and we have a divisor k
+     * - Goal: Split tree into maximum components where each component's sum is divisible by k
+     *
+     * KEY INSIGHT:
+     * Think in terms of NODES, not edges!
+     * - Process tree from leaves inward (like peeling an onion)
+     * - For each leaf: if its value is divisible by k, it can form its own component
+     * - If not divisible, merge it with its parent (add values together)
+     * - Continue until all nodes are processed
+     *
+     * APPROACH:
+     * 1. Build adjacency list and track node degrees (number of connections)
+     * 2. Use BFS starting from all leaf nodes (degree = 1)
+     * 3. For each leaf:
+     *    - If sum is divisible by k → it's a valid component (increment count)
+     *    - If not divisible by k → merge with parent (add value to parent)
+     * 4. After processing, the parent becomes a new leaf (repeat)
+     *
+     * TIME: O(n) - visit each node once
+     * SPACE: O(n) - adjacency list and auxiliary arrays
      */
+
 public:
-    int maxKDivisibleComponents(const int n, const vector<vector<int>> &edges, const vector<int> &values, const int k)
+    int maxKDivisibleComponents(int n,
+                                const vector<vector<int>> &edges,
+                                const vector<int> &values,
+                                int k)
     {
+        // Base case: single node is always valid
         if (n < 2)
             return 1;
-        vvi adj(n);
-        vi indegree(n);
-        vl nodeValues(all(values));
+
+        // Build adjacency list for the tree
+        vector<vector<int>> adjacencyList(n);
+        vector<int> degree(n, 0); // Track connections for each node
+        vector<long long> nodeValues(values.begin(), values.end());
+
+        // Build undirected graph
         for (const auto &edge : edges)
         {
-            adj.at(edge[0]).eb(edge[1]);
-            adj.at(edge[1]).eb(edge[0]);
-            indegree.at(edge[0])++; // undirected edge
-            indegree.at(edge[1])++;
+            int node1 = edge[0];
+            int node2 = edge[1];
+            adjacencyList[node1].push_back(node2);
+            adjacencyList[node2].push_back(node1);
+            degree[node1]++;
+            degree[node2]++;
         }
-        queue<int> q;
-        forsn(i, 0, n)
+
+        // Initialize BFS queue with all leaf nodes (degree == 1)
+        queue<int> leafQueue;
+        for (int i = 0; i < n; i++)
         {
-            if (indegree[i] == 1)
+            if (degree[i] == 1)
             {
-                q.push(i);
+                leafQueue.push(i);
             }
         }
 
         int componentCount = 0;
-        while (!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            indegree.at(node)--;
-            const ll valueToAdd = (nodeValues.at(node) % k != 0) ? nodeValues.at(node) : 0;
-            componentCount += (valueToAdd == 0); // this means that the current node is divisible by k
 
-            for (const int &child : adj.at(node))
+        // Process nodes layer by layer from leaves to root
+        while (!leafQueue.empty())
+        {
+            int currentNode = leafQueue.front();
+            leafQueue.pop();
+
+            // Mark node as processed
+            degree[currentNode]--;
+
+            // Check if current node's value is divisible by k
+            long long currentValue = nodeValues[currentNode];
+            bool isDivisible = (currentValue % k == 0);
+
+            if (isDivisible)
             {
-                if (indegree.at(child) > 0)
+                // This node forms its own component - don't propagate value
+                componentCount++;
+            }
+
+            // Propagate value to parent if not divisible
+            long long valueToPropagate = isDivisible ? 0 : currentValue;
+
+            // Find parent and merge values if needed
+            for (int neighbor : adjacencyList[currentNode])
+            {
+                // Only process unvisited neighbors (potential parents)
+                if (degree[neighbor] > 0)
                 {
-                    // we have removed the leaf node hence the indegree will reduce by 1
-                    indegree.at(child)--;
-                    nodeValues.at(child) += valueToAdd; // make sure to combine(merge) the values of the nodes
-                    if (indegree.at(child) == 1)
-                    { // this means that the current node is a leaf
-                        q.emplace(child);
+                    // Remove edge: reduce neighbor's degree
+                    degree[neighbor]--;
+
+                    // Merge current node's value into parent
+                    nodeValues[neighbor] += valueToPropagate;
+
+                    // If neighbor becomes a leaf, add to queue
+                    if (degree[neighbor] == 1)
+                    {
+                        leafQueue.push(neighbor);
                     }
                 }
             }
         }
+
         return componentCount;
     }
 };
 
-int main(int argc, char const *argv[])
+/*
+ * EXAMPLE WALKTHROUGH:
+ * n = 5, edges = [[0,2],[1,2],[1,3],[2,4]], values = [1,8,1,4,4], k = 6
+ *
+ * Tree structure:
+ *       0(1)
+ *        |
+ *       2(1)
+ *      / |
+ *    1(8) 4(4)
+ *     |
+ *    3(4)
+ *
+ * Initial leaves: 0, 3, 4 (degree = 1)
+ *
+ * Process leaf 0: value=1, not divisible by 6 → merge with parent 2
+ *   nodeValues[2] = 1 + 1 = 2
+ *
+ * Process leaf 3: value=4, not divisible by 6 → merge with parent 1
+ *   nodeValues[1] = 8 + 4 = 12 (divisible!) → component++
+ *
+ * Process leaf 4: value=4, not divisible by 6 → merge with parent 2
+ *   nodeValues[2] = 2 + 4 = 6 (divisible!) → component++
+ *
+ * Result: 2 components
+ */
+int main()
 {
-    Solution obj;
-    cout
-        << "\n\n\tProgram ended\n\n";
     return 0;
 }
